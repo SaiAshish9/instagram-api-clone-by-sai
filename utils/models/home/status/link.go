6e9e16f10c6a3418e0dd5713ff19778a1e89c8de
saiashish9 <saiashish3760@gmail.com>
2021-02-03 23:49:51 +0530

    status controller updated

--- utils/models/home/status/link.go
+++ utils/models/home/status/link.go
@@ -1,8 +1,12 @@
 package status
 
 type Link struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Image string `json:"url"`
-	Msg   string `json:"msg"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Image       string `json:"url"`
+	Msg         string `json:"msg"`
+	StatusImg   string `json:"status-img"`
+	StatusVideo string `json:"status-video"`
+	StatusMsg   string `json:"status-msg"`
+	Time        string `json:"time"`
 }
